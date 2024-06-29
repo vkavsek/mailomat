bb592e98118a404c081640f84cae39a2dd461763
vkavsek <vkavsek@gmail.com>
2024-06-29 16:24:01 +0200

    added ssl options to db_config

--- src/config/structs.rs
+++ src/config/structs.rs
@@ -7,7 +7,7 @@ use derive_more::From;
 use lazy_regex::regex_captures;
 use secrecy::{ExposeSecret, SecretString};
 use serde::{Deserialize, Serialize};
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 use strum_macros::AsRefStr;
 use toml::Value;
 
@@ -60,6 +60,7 @@ pub struct DbConfig {
     pub port: u16,
     pub host: String,
     pub db_name: String,
+    pub require_ssl: bool,
 }
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct AppConfigBuilder(HashMap<String, HashMap<String, Value>>);
@@ -78,11 +79,19 @@ impl DbConfig {
         self.connection_options_without_db().database(&self.db_name)
     }
     pub fn connection_options_without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // If explanation needed: https://fly.io/docs/networking/private-networking/
+            PgSslMode::Disable
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 }
 
@@ -137,8 +146,8 @@ impl TryFrom<&str> for DbConfig {
     type Error = crate::Error;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        // postgres://{username}:{password}@{hostname}:{port}/{database}
-        let (_whole, username, password, host, port, db_name, _options) = regex_captures!(
+        // postgres://{username}:{password}@{hostname}:{port}/{database}?{options}
+        let (_whole, username, password, host, port, db_name, options) = regex_captures!(
             r#"^postgres:\/\/([^:]+):([^@]+)@([^:\/]+):(\d+)\/([^\s\/?]+)(\?[^\s]*)?$"#,
             value
         )
@@ -151,51 +160,27 @@ impl TryFrom<&str> for DbConfig {
             .parse()
             .map_err(|_| crate::Error::StringToDbConfigFail)?;
 
+        let mut require_ssl = true;
+        if let Some(options) = options.strip_prefix('?') {
+            for option in options.split(',') {
+                if let Some((id, val)) = option.split_once('=') {
+                    if id == "sslmode" && val == "disable" {
+                        require_ssl = false;
+                    }
+                }
+            }
+        }
+
         Ok(DbConfig {
             username,
             password,
             port,
             host,
             db_name,
+            require_ssl,
         })
     }
 }
-
-// FIXME: DELETE
-// impl TryFrom<AppConfigBuilder> for NetConfig {
-//     type Error = ConfigError;
-//     fn try_from(value: AppConfigBuilder) -> Result<Self, Self::Error> {
-//         let net_config_hm = value
-//             .0
-//             .get("net_config")
-//             .ok_or(ConfigError::NetConfigBuildFail)?;
-//         let host = net_config_hm
-//             .get("host")
-//             .and_then(|val| val.as_array())
-//             .and_then(|arr| {
-//                 if arr.len() == 4 {
-//                     let mut host_arr = [0u8; 4];
-//                     for (i, val) in arr.iter().enumerate() {
-//                         if let Some(val) = val.as_integer().and_then(|val| val.try_into().ok()) {
-//                             host_arr[i] = val;
-//                         } else {
-//                             return None;
-//                         }
-//                     }
-//                     Some(host_arr)
-//                 } else {
-//                     None
-//                 }
-//             })
-//             .ok_or(ConfigError::NetConfigBuildFail)?;
-//         let app_port = net_config_hm
-//             .get("app_port")
-//             .and_then(|val| val.as_integer())
-//             .and_then(|val| val.try_into().ok())
-//             .ok_or(ConfigError::NetConfigBuildFail)?;
-//         Ok(NetConfig { host, app_port })
-//     }
-// }
 
 // ###################################
 // ->   TESTS
@@ -207,36 +192,6 @@ mod tests {
 
     use super::*;
     use crate::Result;
-
-    // FIXME: DELETE
-    // #[test]
-    // fn test_try_from_app_config_builder_for_net_config_success(
-    // ) -> core::result::Result<(), ConfigError> {
-    //     let test_net_config = NetConfig {
-    //         host: [127, 0, 0, 1],
-    //         app_port: 8080,
-    //     };
-    //     let mut app_config_builder_hm = HashMap::new();
-    //     app_config_builder_hm.insert(
-    //         "net_config".to_string(),
-    //         HashMap::from([
-    //             (
-    //                 "host".to_string(),
-    //                 Value::Array(vec![
-    //                     Value::Integer(127),
-    //                     Value::Integer(0),
-    //                     Value::Integer(0),
-    //                     Value::Integer(1),
-    //                 ]),
-    //             ),
-    //             ("app_port".to_string(), Value::Integer(8080)),
-    //         ]),
-    //     );
-    //     let app_config_builder = AppConfigBuilder(app_config_builder_hm);
-    //     let net_config = NetConfig::try_from(app_config_builder)?;
-    //     assert_eq!(test_net_config, net_config);
-    //     Ok(())
-    // }
 
     #[test]
     fn test_app_config_add_source_and_build_ok() -> ConfigResult<()> {
@@ -256,6 +211,7 @@ mod tests {
                 port: 5432,
                 host: "127.0.0.1".to_string(),
                 db_name: "newsletter".to_string(),
+                require_ssl: false,
             },
         };
 
@@ -286,6 +242,18 @@ mod tests {
     #[test]
     fn test_db_config_from_str_ok() -> Result<()> {
         {
+            let db_url = "postgres://my_uname:pwd@localhost:6666/my_db?sslmode=disable";
+            let db_config = DbConfig::try_from(db_url)?;
+
+            assert_eq!("my_uname", db_config.username);
+            assert_eq!("pwd", db_config.password.expose_secret());
+            assert_eq!("localhost", db_config.host);
+            assert_eq!(6666, db_config.port);
+            assert_eq!("my_db", db_config.db_name);
+            assert!(!db_config.require_ssl);
+        }
+
+        {
             let db_url = "postgres://my_uname:pwd@localhost:6666/my_db";
             let db_config = DbConfig::try_from(db_url)?;
 
@@ -294,17 +262,7 @@ mod tests {
             assert_eq!("localhost", db_config.host);
             assert_eq!(6666, db_config.port);
             assert_eq!("my_db", db_config.db_name);
-        }
-
-        {
-            let db_url = "postgres://my_uname:pwd@localhost:6666/my_db?ssl=disable";
-            let db_config = DbConfig::try_from(db_url)?;
-
-            assert_eq!("my_uname", db_config.username);
-            assert_eq!("pwd", db_config.password.expose_secret());
-            assert_eq!("localhost", db_config.host);
-            assert_eq!(6666, db_config.port);
-            assert_eq!("my_db", db_config.db_name);
+            assert!(db_config.require_ssl);
         }
 
         Ok(())

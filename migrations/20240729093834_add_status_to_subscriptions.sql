72e7d194f3175da9310fe94712754ed18290c1e3
vkavsek <vkavsek@gmail.com>
2024-07-29 14:40:39 +0200

    DB migration - make status not null in subscriptions

--- migrations/20240729093834_add_status_to_subscriptions.sql
+++ migrations/20240729093834_add_status_to_subscriptions.sql
@@ -1,7 +1,2 @@
 -- Add migration script here
 ALTER TABLE subscriptions ADD COLUMN status TEXT NULL;
-
--- CREATE TABLE subscription_tokens (
--- 	token UUID NOT NULL PRIMARY KEY,
--- 	id UUID NOT NULL,
--- )

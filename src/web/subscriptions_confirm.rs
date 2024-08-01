7aafb5cca88081e96583e08c46150387d324ba95
vkavsek <vkavsek@gmail.com>
2024-08-01 19:05:46 +0200

    feat: first implementation of subscription confirmation

--- src/web/subscriptions_confirm.rs
+++ src/web/subscriptions_confirm.rs
@@ -1,14 +1,17 @@
+use std::ops::Deref;
+
 use axum::{
     extract::{Query, State},
     http::StatusCode,
 };
+use derive_more::Deref;
 use serde::Deserialize;
 
 use crate::AppState;
 
 use super::Result;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Deref)]
 pub struct SubscribeConfirmQuery {
     subscription_token: String,
 }
@@ -16,7 +19,37 @@ pub struct SubscribeConfirmQuery {
 #[tracing::instrument(name = "Confirm a pending subscriber", skip(app_state))]
 pub async fn confirm(
     State(app_state): State<AppState>,
-    query: Query<SubscribeConfirmQuery>,
+    Query(subscription_token): Query<SubscribeConfirmQuery>,
 ) -> Result<StatusCode> {
+    let db_pool = app_state.mm.db();
+
+    // Get the subscriber_id record from the database.
+    let sub_id_record = sqlx::query!(
+        r#"SELECT subscriber_id FROM subscription_tokens
+    WHERE subscription_token = $1"#,
+        subscription_token.deref()
+    )
+    .fetch_one(db_pool)
+    .await?;
+    let subscriber_id = sub_id_record.subscriber_id;
+
+    // Update the status of the subscriber
+    sqlx::query!(
+        r#"UPDATE subscriptions 
+        SET status = 'confirmed' WHERE id = $1"#,
+        subscriber_id
+    )
+    .execute(db_pool)
+    .await?;
+
+    // Delete the entry from the subscription_tokens table
+    sqlx::query!(
+        r#"DELETE FROM subscription_tokens
+    WHERE subscription_token = $1"#,
+        subscription_token.deref()
+    )
+    .execute(db_pool)
+    .await?;
+
     Ok(StatusCode::OK)
 }

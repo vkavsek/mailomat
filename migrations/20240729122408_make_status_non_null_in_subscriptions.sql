4abb01be693a63db044e162f3c228ba155c6ae56
Vid Desktop <vkavsek@gmail.com>
2025-05-12 12:53:40 +0200

    style: sql formatting

--- migrations/20240729122408_make_status_non_null_in_subscriptions.sql
+++ migrations/20240729122408_make_status_non_null_in_subscriptions.sql
@@ -4,9 +4,10 @@
 BEGIN;
 	-- Backfill `status` for historical entries
 	UPDATE subscriptions
-		SET status = 'confirmed'
-		WHERE status IS NULL; 
+  SET status = 'confirmed'
+  WHERE status IS NULL; 
+
 	-- Make `status` mandatory
 	ALTER TABLE subscriptions 
-    ALTER COLUMN status SET NOT NULL;
+  ALTER COLUMN status SET NOT NULL;
 COMMIT;
